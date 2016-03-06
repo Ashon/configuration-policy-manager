bf0b35dffc6dfb31432becefa0fed6f6f9549724
ashon <ashon8813@gmail.com>
2016-03-06 12:35:34 +0900

    add url

--- src/dcpm/config_policy/tests/__init__.py
+++ src/dcpm/config_policy/tests/__init__.py
@@ -2,4 +2,5 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'dcpm.settings'
 import django
+
 django.setup()
--- src/dcpm/config_policy/urls.py
+++ src/dcpm/config_policy/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from dcpm.config_policy import views
